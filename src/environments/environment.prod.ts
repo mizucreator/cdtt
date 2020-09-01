479e2daaaf6c13620d05fd65b5a64a8862987db3
Nguyễn Trung Nhân <nhanntdl98@gmail.com>
2020-09-01 13:32:03 +0700

    install firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firbaseConfig: {
+    apiKey: 'AIzaSyBQQPgKkRBDiikcEEMV4GCe_JgQ_JRrF60',
+    authDomain: 'cdtt-6835f.firebaseapp.com',
+    databaseURL: 'https://cdtt-6835f.firebaseio.com',
+    projectId: 'cdtt-6835f',
+    storageBucket: 'cdtt-6835f.appspot.com',
+    messagingSenderId: '219261445678',
+    appId: '1:219261445678:web:a33d0d7ab38f970c1761bf',
+    measurementId: 'G-6VFNFQ3212',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firbaseConfig: {
+    apiKey: 'AIzaSyBQQPgKkRBDiikcEEMV4GCe_JgQ_JRrF60',
+    authDomain: 'cdtt-6835f.firebaseapp.com',
+    databaseURL: 'https://cdtt-6835f.firebaseio.com',
+    projectId: 'cdtt-6835f',
+    storageBucket: 'cdtt-6835f.appspot.com',
+    messagingSenderId: '219261445678',
+    appId: '1:219261445678:web:a33d0d7ab38f970c1761bf',
+    measurementId: 'G-6VFNFQ3212',
+  },
 };
 
 /*
